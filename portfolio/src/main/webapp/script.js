44f42326156874acd6752fc4703a69b3db73ff4c
Ayomide Adekunle <davidadekunle88@gmail.com>
2020-06-11 14:50:26 +0000

    requests information from server and displays it in the html page

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -26,3 +26,9 @@ function addRandomFact() {
   const factContainer = document.getElementById('fact-container');
   factContainer.innerText = fact;
 }
+
+function printHelloMessage() {
+    fetch('/data').then(response => response.text()).then((message) => {
+        document.getElementById('message-container').innerText = message;
+    });
+}
